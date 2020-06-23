4d80afe176fe8c4d0707df695b70c3c1971cd759
zhopen <you@example.com>
2020-06-23 10:01:46 +0800

    hello action add

---
+++ addressbook/addressbook.clauses.md
@@ -0,0 +1,5 @@
+<h1 class="clause"> Main Info </h1>
+
+version: 1.0.0
+cdt: v1.6.2
+title: addressbook
--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,5 +1,5 @@
-#include <eosiolib/eosio.hpp>
-#include <eosiolib/print.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/print.hpp>
 
 
 using namespace eosio;
@@ -7,19 +7,26 @@ using namespace eosio;
 class hello : public contract {
   public:
     using contract::contract;
-
+     
     [[eosio::action]]
     void hi( name user ) {
 	    require_auth(user);
-        print( "Hello, ", name{user});
-    	//eosio_assert(0, "哈啊哈{ab,c,c,c,c,c,c,}");
+	    print("get_first_receiver()=", get_first_receiver());
+
+		print( "Hello, ", name{user});
+    	//eosio_assert(1, "哈啊哈{ab,c,c,c,c,c,c,}");
       }
 	  
 	[[eosio::action]]
-     void hi2() {
+     void hiself() {
 		require_auth(_self);
 		print( "Hello, ", _self.to_string());
 
+   }
+   
+   	[[eosio::action]]
+     void hiany() {
+		print( "Hello, ", _self.to_string());
    }
 };
 //EOSIO_DISPATCH( hello, (hi)(hi2))
